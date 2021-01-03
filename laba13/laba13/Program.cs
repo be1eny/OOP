8bbce1077bae68c2b609d5169cb5fdbb2dccf8a1
be1eny <alex.samtsevich@gmail.com>
2021-01-03 16:46:36 +0300

    all labs yes

--- laba13/laba13/Program.cs
+++ laba13/laba13/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-
-namespace laba13
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace lab13
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TASDiskInfo diskInfo = new TASDiskInfo();
+            diskInfo.DiskInfo();
+            TASFileInfo fileInfo = new TASFileInfo();
+            fileInfo.FileData(@"/Users/Alex/Univercity/OOP/laba13/laba13/Class1.cs");
+            TASDirInfo dirInfo = new TASDirInfo();
+            dirInfo.DirInfo(@"/Users/Alex/Univercity");
+            TASFileManager fileManager = new TASFileManager();
+            fileManager.FileManager("/Users");
+            TASLog.SearchByString("FileInfo:");
         }
     }
 }
